ff3a1b31941cce85f082c339c92e626dede15187
chankruze <chankruze@gmail.com>
2019-05-17 16:44:16 +0530

    RMX1851: Add vendor libs & run commands files
    Signed-off-by: chankruze <chankruze@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,7 +91,6 @@ TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO := true
 TW_CRYPTO_FS_TYPE := "ext4"
 TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/bootdevice/by-name/userdata"
 TW_CRYPTO_MNT_POINT := "/data"
