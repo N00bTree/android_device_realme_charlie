02a68d3020727dc2b667fa5de3bae015efc32052
chankruze <chankruze@gmail.com>
2019-06-04 19:58:54 +0530

    RMX1851: Update Device Tree
    * add uevent run command file
    * update kernel
    * update recovery fstab
    * update BoardConfig flags
    * update system props
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,6 @@ TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
-TARGET_USES_64_BIT_BINDER := true
 
 ENABLE_CPUSETS := true
 ENABLE_SCHEDBOOST := true
@@ -21,7 +20,6 @@ ENABLE_SCHEDBOOST := true
 # Platform
 TARGET_BOARD_PLATFORM := sdm710
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno616
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm710
@@ -29,19 +27,42 @@ TARGET_NO_BOOTLOADER := true
 TARGET_USES_UEFI := true
 
 # Kernel
-BOARD_KERNEL_BASE := 0x00000000
+# BOARD_KERNEL_OFFSET := 0x00008000
+# BOARD_RECOVERY_DTBO_SIZE := 79027
+# BOARD_RECOVERY_DTBO_OFFSET := 33603584 # int64_t
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_BOOTIMG_HEADER_VERSION := 1
+BOARD_HEADER_SIZE := 1648
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+# Kernel offsets
+BOARD_KERNEL_BASE := 0x00000000
 BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_KERNEL_CMDLINE := ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA90000 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3
-BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=1 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_SECOND_OFFSET := 0x00f00000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+
+# Kernel CMDLINE
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8
+BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0xA90000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += swiotlb=1
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
-BOARD_MKBOOTIMG_ARGS := --base 0x00000000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
+BOARD_KERNEL_CMDLINE += skip_override androidboot.fastboot=1
+# Prebuilt kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image.gz-dtb
+# Make boot image arguments
+BOARD_MKBOOTIMG_ARGS := --base $(BOARD_KERNEL_BASE) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --second_offset $(BOARD_SECOND_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
 # system.prop
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
@@ -52,6 +73,8 @@ BOARD_FLASH_BLOCK_SIZE := 262144
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+TARGET_NO_KERNEL := false
+TARGET_NO_RECOVERY := false
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 # Workaround for error copying vendor files to recovery ramdisk
@@ -59,53 +82,78 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_SUPPRESS_SECURE_ERASE := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-
-# TWRP
 BOARD_USE_FRAMEBUFFER_ALPHA_CHANNEL := true
-DEVICE_SCREEN_WIDTH := 1080
-DEVICE_SCREEN_HEIGHT := 2340
 RECOVERY_SDCARD_ON_DATA := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
-TARGET_DISABLE_TRIPLE_BUFFERING := false
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
+TARGET_USES_MKE2FS := true
+
+# Qcom specific fixes
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
+# Recovery device modules
+TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
+
+########
+# TWRP #
+########
+
+DEVICE_SCREEN_WIDTH := 1080
+DEVICE_SCREEN_HEIGHT := 2340
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_NO_BATT_PERCENT := false
+TW_USE_TOOLBOX := true
+TW_IGNORE_MISC_WIPE_DATA := true
+TW_HAS_EDL_MODE := true
+
+# Display
 TW_THEME := portrait_hdpi
+# DEVICE_RESOLUTION := 1080x2340 (deprecated)
 TW_DEFAULT_BRIGHTNESS := 188
 TW_MAX_BRIGHTNESS := 1023
+TW_Y_OFFSET := 80
+TW_H_OFFSET := -80
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_NO_SCREEN_BLANK := true
+TW_NO_SCREEN_TIMEOUT := true
+TW_USE_LEDS_HAPTICS := true
+
+# TWRP
+
+TARGET_DISABLE_TRIPLE_BUFFERING := false
+TW_THEME := portrait_hdpi
+
 TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone4/temp
-TW_BRIGHTNESS_PATH := /sys/class/backlight/panel0-backlight/brightness
+
+# Storage & FS
+TW_HAS_MTP := true
+TW_MTP_DEVICE := /dev/mtp_usb
 TW_INTERNAL_STORAGE_PATH := "/data/media"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_EXTRA_LANGUAGES := true
-TW_DEFAULT_LANGUAGE := en_EN
 TW_NO_USB_STORAGE := false
-TW_NO_SCREEN_BLANK := true
-TW_NO_SCREEN_TIMEOUT := true
-TW_NO_BATT_PERCENT := false
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_USE_TOOLBOX := true
-TW_USE_QCOM_HAPTICS_VIBRATOR := true
-TW_IGNORE_MISC_WIPE_DATA := true
+TW_INCLUDE_NTFS_3G := true
+
+# Locale
+TW_EXTRA_LANGUAGES := true
+TW_DEFAULT_LANGUAGE := en
+
+# Excludes
 TW_EXCLUDE_SUPERSU := true
 TW_EXCLUDE_TWRPAPP := true
-TW_HAS_EDL_MODE := true
-TW_Y_OFFSET := 80
-TW_H_OFFSET := -80
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_INCLUDE_REPACKTOOLS := true
 
-# Crypto
+# Encryption
+PLATFORM_SECURITY_PATCH := 2019-05-05
 TW_INCLUDE_CRYPTO := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/bootdevice/by-name/userdata"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,barrier=0,data=ordered"
 
 # Debug
+TARGET_USES_LOGD := true
 TWRP_INCLUDE_LOGCAT := true
