86da2ae58dd053c2b624dc82019735f0a8e4500c
HARSH <48650798+hpnightowl@users.noreply.github.com>
2019-06-05 08:14:24 +0530

    charlie: it's A 64bit device it will need it :)

--- omni_charlie.mk
+++ omni_charlie.mk
@@ -2,6 +2,9 @@
 PRODUCT_RELEASE_NAME := RMX1851
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our custom product configuration
