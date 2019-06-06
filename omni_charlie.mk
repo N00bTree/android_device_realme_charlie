4f4fcc9c1b40e993d731509008aaad800620158d
chankruze <chankruze@gmail.com>
2019-06-06 19:55:37 +0000

    RMX1851: Adapt With Pitch Black Recovery Project
    Signed-off-by: chankruze <chankruze@gmail.com>

--- omni_charlie.mk
+++ omni_charlie.mk
@@ -1,11 +1,10 @@
 # Release name
 PRODUCT_RELEASE_NAME := RMX1851
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Realme
